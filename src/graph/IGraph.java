153661973e017eb24a152185798062c1f57bd0d5
Juan Reyes <seyerman@gmail.com>
2019-10-23 13:29:48 -0500

    IGraph interface added to graph package with the first proposal of basic graph operations

---
+++ src/graph/IGraph.java
@@ -0,0 +1,14 @@
+package graph;
+
+import java.util.List;
+
+public interface IGraph<V> {
+	public boolean addVertex(V v);
+	public void addEdge(V u, V v);
+	public void addEdge(V u, V v, double w);
+	public boolean removeVertex(V v);
+	public void removeEdge(V u, V v);
+	public List<V> vertexAdjacent(V v);
+	public boolean areConnected(V u, V v);
+	public double[][] weightMatrix();
+}
