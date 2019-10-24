52c8450bd397f050e248d959842e61829f2e2d20
Jhon Edward Mora <jhonedmora@gmail.com>
2019-10-24 09:44:55 -0500

    Commented method traversal and did some attemps on its logic without results

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -7,6 +7,8 @@ import java.util.*;
  */
 public class GraphAlgorithms{
 	
+	
+	
 	public static <V> List<V> bfs(IGraph<V> g, V v){
 		return null; //TODO Create method
 	}
@@ -14,8 +16,20 @@ public class GraphAlgorithms{
 	public static <V> List<V> dfs (IGraph<V> g, V v){
 		return null; //TODO Create method
 	}
-	
+
+	/**
+	 * This method will traverse the graph given a data structure. This will perform  BFS or DFS, given the case.
+	 * @param g The graph to be traversed.
+	 * @param v The vertex from which the traversal will begin.
+	 * @param ds The data structure to be used in this traversal. Either a Stack for a DFS or a Queue for BFS. Must be empty.
+	 * @return A List with the resulting traversal performed on the given graph from the given vertex.
+	 */
 	private static <V> List<V> traversal(IGraph<V> g, V v, List<V> ds){
+
+		V vertex = v;
+		while(!ds.isEmpty()) {
+			
+		}
 		return null; //TODO Create method
 	}
 	
