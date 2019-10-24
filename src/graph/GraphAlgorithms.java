8557f87555d2a06ae7b8a2bba987e5a96bc422fb
Jhon Edward Mora <jhonedmora@gmail.com>
2019-10-24 09:09:06 -0500

    Created signature for methods in class GraphAlgorithms. Still can't compile

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -1,4 +1,5 @@
 package graph;
+import java.util.*;
 
 /**
  * This class is meant to execute different algorithms on graphs. 
@@ -6,5 +7,19 @@ package graph;
  */
 public class GraphAlgorithms {
 
+	public List<V> bfs(IGraph<V> g, V v){
+		
+	}
 	
+	public List<V> dfs (IGraph<V> g, V v){
+		
+	}
+	
+	private List<V> traversal(IGraph<V> g, V v, List<V> ds){
+		
+	}
+	
+	private Map<V, Double> dijkstra(IGraph<V> g, V v){
+		
+	}
 }
