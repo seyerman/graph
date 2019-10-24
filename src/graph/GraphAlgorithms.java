aab68b350daf4cb7d02b028fe75b5cd0014e6953
Jhon Edward Mora <jhonedmora@gmail.com>
2019-10-24 09:32:06 -0500

    Corrected errors on class GraphAlgorithms. Still a draft, though

--- src/graph/GraphAlgorithms.java
+++ src/graph/GraphAlgorithms.java
@@ -5,21 +5,21 @@ import java.util.*;
  * This class is meant to execute different algorithms on graphs. 
  * @version 1.0 - 10/2019
  */
-public class GraphAlgorithms {
-
-	public List<V> bfs(IGraph<V> g, V v){
-		
+public class GraphAlgorithms{
+	
+	public static <V> List<V> bfs(IGraph<V> g, V v){
+		return null; //TODO Create method
 	}
 	
-	public List<V> dfs (IGraph<V> g, V v){
-		
+	public static <V> List<V> dfs (IGraph<V> g, V v){
+		return null; //TODO Create method
 	}
 	
-	private List<V> traversal(IGraph<V> g, V v, List<V> ds){
-		
+	private static <V> List<V> traversal(IGraph<V> g, V v, List<V> ds){
+		return null; //TODO Create method
 	}
 	
-	private Map<V, Double> dijkstra(IGraph<V> g, V v){
-		
+	private static <V> Map<V, Double> dijkstra(IGraph<V> g, V v){
+		return null; //TODO Create method
 	}
 }
